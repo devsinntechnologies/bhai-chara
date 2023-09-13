13f3587c21e01b8cefbdb31af0c0595ea23faefc
kashaf172725 <kashafdevsinn@gmail.com>
2023-09-13 06:37:53 -0700

    .

--- lib/view/chat_list_screen.dart
+++ lib/view/chat_list_screen.dart
@@ -12,50 +12,52 @@ class ChattListScreen extends StatelessWidget {
       appBar: AppBar(
           backgroundColor:AppColors.black,
           
-        title: const Text("Chats"),
+        title:  Text("Chats"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width ,
-                  height: 50,
-                  child: TextFormField(
-                      decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    hintText: "Type here",
-                    hintStyle: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w400),
-                  )),
-                ),
-              ),
-              for (int i = 0; i <= 10; i++)
-                ListTile(
-                  leading: CircleAvatar(radius: 25),
-                  title: Text(
-                    "Kashaf Waheed",
-                    style: AppTextStyles.textStyleNormalBodyMedium,
-                  ),
-                  subtitle: Text(
-                    "hello",
-                    style: AppTextStyles.textStyleNormalBodyXSmall
-                        .copyWith(color: AppColors.grey),
-                  ),
-                  trailing: Text(
-                    "3/7/23",
-                    style: AppTextStyles.textStyleNormalBodyXSmall
-                        .copyWith(color: AppColors.grey),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(15.0),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: [
+      //         Padding(
+      //           padding: const EdgeInsets.symmetric(vertical: 12.0),
+      //           child: Container(
+      //             width: MediaQuery.of(context).size.width ,
+      //             height: 50,
+      //             child: TextFormField(
+      //                 decoration: InputDecoration(
+      //               border: OutlineInputBorder(
+      //                   borderRadius: BorderRadius.circular(15)),
+      //               hintText: "Type here",
+      //               hintStyle: const TextStyle(
+      //                   fontSize: 16, fontWeight: FontWeight.w400),
+      //             )),
+      //           ),
+      //         ),
+      //         for (int i = 0; i <= 10; i++)
+      //           ListTile(
+      //             leading: CircleAvatar(radius: 25),
+      //             title: Text(
+      //               "Kashaf Waheed",
+      //               style: AppTextStyles.textStyleNormalBodyMedium,
+      //             ),
+      //             subtitle: Text(
+      //               "hello",
+      //               style: AppTextStyles.textStyleNormalBodyXSmall
+      //                   .copyWith(color: AppColors.grey),
+      //             ),
+      //             trailing: Text(
+      //               "3/7/23",
+      //               style: AppTextStyles.textStyleNormalBodyXSmall
+      //                   .copyWith(color: AppColors.grey),
+      //             ),
+      //           ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+   
+   
     );
   }
 }
