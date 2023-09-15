a66bc04bff5400f0a0119bb2d557ee6bb571b27e
kashaf172725 <kashafdevsinn@gmail.com>
2023-09-15 04:13:09 -0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bhai_chara/provider/splash_provider.dart';
 import 'package:bhai_chara/tempoprovider.dart';
+import 'package:bhai_chara/view/chat_list_screen.dart';
 import 'package:bhai_chara/view/live_chatt_screen.dart';
 import 'package:bhai_chara/view/review_details_screen.dart';
 import 'package:flutter/material.dart';
