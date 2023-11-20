f2ce90a1df895d0f003ced962f013fc35c02a604
kashaf172725 <kashafdevsinn@gmail.com>
2023-11-19 17:42:39 -0800

    .

--- lib/view/chatting/view/chat_view.dart
+++ lib/view/chatting/view/chat_view.dart
@@ -2,11 +2,8 @@ import 'package:bhai_chara/view/chatting/controller/provider/chat_provider.dart'
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
-import 'package:bhai_chara/view/chatting/view/widget/chat_tile_widget.dart';
-import 'package:bhai_chara/view/chatting/model/chattUser.dart';
+import 'widget/chat_tile_widget.dart';
 
-import 'dart:convert';
-import 'package:bhai_chara/view/chatting/api/apis.dart';
 class ChatView extends StatefulWidget {
   const ChatView({super.key});
 
@@ -15,7 +12,6 @@ class ChatView extends StatefulWidget {
 }
 
 class _ChatViewState extends State<ChatView> {
-    List<ChattUser> list = [];
   FocusNode searchFN = FocusNode();
   TextEditingController searchController = TextEditingController();
   PageController pageMove = PageController();
@@ -35,32 +31,18 @@ class _ChatViewState extends State<ChatView> {
               child: searchField(),
             ),
             Expanded(
-              child:StreamBuilder(
-                stream: APIS.firestore.collection('users').snapshots(),
-                builder: (context,snapshot){
-                  switch(snapshot.connectionState){
-                    case ConnectionState.waiting:
-                    case ConnectionState.none:
-                          return const Center(child: CircularProgressIndicator());
-                    case ConnectionState.active:
-                    case ConnectionState.done:
-                               
-                  
-                    final data = snapshot.data!.docs;
-                  list = data?.map((e) => ChattUser.fromJson(e.data())).toList()??[];
-                  
-                  return  ListView.builder(
-                itemCount: list.length,
+              child: ListView.builder(
+                itemCount: chatVM.listOfChatModel.length,
                 itemBuilder: (context, index) {
-                  return ChattUserCard(user: list[index],);
-                
+                  return Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.h),
+                    child: ChatTileWidget(
+                      model: chatVM.listOfChatModel[index],
+                    ),
+                  );
                 },
-              );
-
-                  }
-            
-                }
-              )
+              ),
             ),
           ],
         ),
