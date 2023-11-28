b1a5b1e423578940a92bedd5bf4af3d5309287c7
zeeshanmalik0110 <zeeshandevsinn@gmail.com>
2023-11-28 10:39:43 +0500

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bhai_chara/controller/provider/authentication_provider/firebase_signup_provider.dart';
 import 'package:bhai_chara/firebase_options.dart';
-import 'package:bhai_chara/view/authentication/otp_code_screen.dart';
 import 'package:bhai_chara/view/chatting/controller/provider/chat_provider.dart';
 import 'package:bhai_chara/view/onboard_screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
